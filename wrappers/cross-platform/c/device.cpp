a32e16562fe21894e3026fdf6e397de1a5ca17b0
Daniel Vlasenko <danny.vlasenko@gmail.com>
2018-07-06 19:51:38 +0300

    wrappers. created windows c-sharp and c-wrapper projects. header with c-calls for device

---
+++ wrappers/cross-platform/c/device.cpp
@@ -0,0 +1,10 @@
+#include "device.h"
+
+int device_connect(Device*) {
+	
+}
+
+int device_disconnect(Device*) {}
+int device_available_channels(const Device*) {}
+int device_available_commands(const Device*) {}
+int device_available_parameters(const Device*) {}
