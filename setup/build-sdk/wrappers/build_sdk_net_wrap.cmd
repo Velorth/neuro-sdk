18f2e5a9e670133496b6bd319ef00ec8404ccf24
Daniel Vlasenko <danny.vlasenko@gmail.com>
2018-12-13 18:11:32 +0300

    windows wrapper. any cpu platform config instead of x86

--- setup/build-sdk/wrappers/build_sdk_net_wrap.cmd
+++ setup/build-sdk/wrappers/build_sdk_net_wrap.cmd
@@ -1,5 +1,5 @@
-call build_sdk_net_wrap_platform_config.cmd x86 Debug nopause
-call build_sdk_net_wrap_platform_config.cmd x86 Release nopause
+call build_sdk_net_wrap_platform_config.cmd "Any CPU" Debug nopause
+call build_sdk_net_wrap_platform_config.cmd "Any CPU" Release nopause
 call build_sdk_net_wrap_platform_config.cmd x64 Debug nopause
 call build_sdk_net_wrap_platform_config.cmd x64 Release nopause
 if NOT "%1"=="nopause" pause
--- setup/generate-rebuild-all.cmd
+++ setup/generate-rebuild-all.cmd
@@ -1,4 +1,5 @@
-rmdir /Q /S "%NEUROSDK%"
+rmdir /Q /S "%NEUROSDK_64%"
+rmdir /Q /S "%NEUROSDK_86%"
 call generate-all.cmd nopause
 call build-all.cmd nopause
 if NOT "%1"=="nopause" pause
